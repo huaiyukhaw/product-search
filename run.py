e54ea0623d982e86dc1fd4c07177ac6e755ac3a8
huaiyukhaw <huaiyukhaw@gmail.com>
2020-06-19 15:27:55 +0800

    add string matching vs semantic search

---
+++ run.py
@@ -0,0 +1,72 @@
+
+from search import Search
+from flask import Flask, render_template, redirect, url_for, request
+import pandas as pd
+import json
+import pickle
+from sentence_transformers import SentenceTransformer
+import scipy.spatial
+
+app = Flask(__name__)
+app.df = pd.read_csv('data/products.csv', index_col=None)
+app.embedder = SentenceTransformer('bert-base-nli-mean-tokens')
+app.corpus = list(app.df['Product Name'])
+# corpus_embeddings = embedder.encode(corpus)
+with open("data/corpus_embeddings.pkl", "rb") as fp:
+    app.corpus_embeddings = pickle.load(fp)
+app.search = Search(app.df, app.embedder, app.corpus, app.corpus_embeddings)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    search_term = ''
+    result = ''
+    method = 'string'
+    show_images = 'hideImages'
+
+    if request.method == 'POST':
+        search_term = request.form.get('searchTerm', search_term)
+        method = request.form.get('method', method)
+        show_images = request.form.get('showImages', show_images)
+        return redirect(url_for('search', search_term=search_term, method=method, show_images=show_images))
+
+    return render_template('search.html', search_term=search_term, result=result, method=method, show_images=show_images)
+
+
+@app.route('/<method>/<search_term>/<show_images>', methods=['GET', 'POST'])
+def search(search_term, method, show_images):
+    if request.method == 'POST':
+        search_term = request.form.get('searchTerm', search_term)
+        method = request.form.get('method', method)
+        show_images = request.form.get('showImages', 'hideImages')
+        return redirect(url_for('search', search_term=search_term, method=method, show_images=show_images))
+        
+    data = json.loads(app.search(search_term, method))
+    result = []
+
+    for d in data:
+        image_paths = d['Images'].replace('[', '').replace(']', '').split(',')
+        new_image_paths = [image.strip('\"') for image in image_paths]
+
+        result.append({
+            'URL': d['URL'],
+            'Product Name': d['Product Name'],
+            'Product ID': d['Product ID'],
+            'Listing Price': d['Listing Price'],
+            'Sale Price': d['Sale Price'],
+            'Discount': d['Discount'],
+            'Brand': d['Brand'],
+            'Description': d['Description'],
+            'Rating': d['Rating'],
+            'Reviews': d['Reviews'],
+            'Images': new_image_paths,
+            'Last Visited': d['Last Visited']
+        })
+
+    number_of_results = len(result)
+    return render_template('search.html', search_term=search_term, result=result, number_of_results=number_of_results, method=method, show_images=show_images)
+
+@app.errorhandler(404) 
+def not_found(e): 
+  return redirect(url_for('index')) 
+
+app.run(debug=True)
--- search.py
+++ search.py
@@ -1,7 +1,38 @@
 
-class Search(object):
-    def __init__(self):
-        self.search = 'Hello World'
+import pandas as pd
+import scipy.spatial
 
-    def __call__(self):
-        return self.search
+class Search(object):
+    def __init__(self, dataframe, embedder,corpus, corpus_embeddings):
+        self.df = dataframe
+        self.embedder = embedder
+        self.corpus = corpus
+        self.corpus_embeddings = corpus_embeddings
+
+    def __call__(self, search_term, method):
+
+        if method == 'string':
+            new_df = self.df[(self.df['Product Name'].str.contains(search_term, case=False)) | (self.df['Description'].str.contains(search_term, case=False)) | (self.df['Brand'].str.contains(search_term, case=False))].drop_duplicates()
+            return new_df.to_json(orient='records')
+        
+        elif method == 'semantic':
+            query_embeddings = self.embedder.encode([search_term])
+            query_embedding = query_embeddings[0]
+
+            closest_n = 10
+            distances = scipy.spatial.distance.cdist([query_embedding], self.corpus_embeddings, "cosine")[0]
+
+            results = zip(range(len(distances)), distances)
+            results = sorted(results, key=lambda x: x[1])
+            
+            names = []
+            for idx, distance in results[0:closest_n]:
+                names.append(self.corpus[idx].strip())
+
+            names = list(set(names))
+            all_df = []
+            for name in names:
+                all_df.append(self.df[(self.df['Product Name'].str.contains(name))].drop_duplicates())
+
+            new_df = pd.concat(all_df, ignore_index=True).drop_duplicates()
+            return new_df.to_json(orient='records')
